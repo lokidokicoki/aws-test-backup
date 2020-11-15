168fd71eb770b8442e9eca941b00939a5ce1ff95
lokidokicoki <lokidokicoki@gmail.com>
2020-11-15 12:35:35 +0000

    feat(main): check boto3 is install. format using `black`

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import argparse
-import boto3
+
+try:
+    import boto3
+except ImportError:
+    print("Missing required boto3 lib")
+    raise
+
 
 def main():
+    """
+    Parse incoming arguments; require basedir and s3bucket
+    check connectivitiy to target S3 bucket; if unavailable, raise and exit
+    get list of files to process from basedir; if empty, exit
+    process each file:
+        upload to s3
+        on success remove from basedir; else raise and exit
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--debug", help="enable debug output", action="store_true")
+    parser.add_argument(
+        "-d", "--debug", help="enable debug output", action="store_true"
+    )
     parser.add_argument("basedir", help="base directory path to upload from")
     parser.add_argument("s3bucket", help="target S3 bucket name")
-    args = parser.parse_args() 
+    args = parser.parse_args()
 
-    print(f'aws-backup from:{args.basedir} to:{args.s3bucket}')
+    print(f"aws-backup from:{args.basedir} to:{args.s3bucket}")
+
 
 if __name__ == "__main__":
     main()

d4426c96fc8abff6149613ebca7f6cb08eb13546
lokidokicoki <lokidokicoki@gmail.com>
2020-11-15 14:19:09 +0000

    feat(main): set up the file walkerw

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import argparse
-from os import path
+import logging
+from os import path, scandir, walk
+from botocore.exceptions import ClientError
 
 try:
     import boto3
@@ -10,13 +12,39 @@ except ImportError:
     raise
 
 
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket, taken form https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client("s3")
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+        print(response)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
 def main():
     """
     Parse incoming arguments; require basedir and s3bucket
+
     check connectivitiy to target S3 bucket; if unavailable, raise and exit
     get list of files to process from basedir; if empty, exit
     process each file:
         upload to s3
+        object naming convention: base-dir/location/YYYY/MM/DD/HH/mm-topic.dat
         on success remove from basedir; else raise and exit
     """
     parser = argparse.ArgumentParser()
@@ -34,16 +62,23 @@ def main():
 
     # get s3 client
     s3 = boto3.resource("s3")
+    found = False
 
     # raise an error is the passed bucket is not accessible
-    if args.s3bucket not in s3.buckets.all():
+    for bucket in s3.buckets.all():
+        if bucket.name == args.s3bucket:
+            found = True
+
+    if not found:
         raise RuntimeError(
             f"S3 bucket `{args.s3bucket}` either does not exist or is not accessible!"
         )
 
-
-# data = open('test.jpg', 'rb')
-# s3.Bucket('my-bucket').put_object(Key='test.jpg', Body=data)
+    # we have got this far, so the basedir and s3bucket are 'valid', now to get the file list
+    # r=root, d=directories, f = files
+    for r, d, f in walk(args.basedir):
+        for file in f:
+            print(path.join(r, file))
 
 
 if __name__ == "__main__":
